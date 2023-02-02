a840e3d54ae6cd8fcf13bbd9f3c859a843e969f3
Jake Shaw <jake541@gmail.com>
2023-02-02 14:47:37 -0800

    changes

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
 import feedparser
+import mastodon
 import re
 import sys
-import tweepy
 import config as cfg
 
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
+from mastodon import Mastodon
 
 # Fortinet RSS feed to parse.
 feed = 'https://pub.kb.fortinet.com/rss/firmware.xml'
@@ -17,12 +18,10 @@ past_hour = datetime.now() - timedelta(hours = 1)
 past_week = datetime.now() - timedelta(days = 7)
 past_month = datetime.now() - timedelta(days = 30)
 
-# Authenticate to Twitter
-auth = tweepy.OAuthHandler(cfg.twitter["consumer_key"], cfg.twitter["consumer_secret"])
-auth.set_access_token(cfg.twitter["access_token"], cfg.twitter["access_token_secret"])
-api = tweepy.API(auth)
-
 d = feedparser.parse(feed)
+
+# Do the Mastodon.
+m = Mastodon(access_token = cfg.mastodon['access_token'], api_base_url = cfg.mastodon['instance'])
 
 # Count how many new entries.
 counter = 0
@@ -43,11 +42,11 @@ for entry in d.entries:
     # If entry date is newer than an hour... (prod = <, debug = >)
     if past_hour < published_dt:
         counter += 1 # Update counter
-        tweet_text = f"Posted: {mo.group(1)}\n\n{entry.published}\n\n{entry.link}"
+        toot_text = f"Posted: {mo.group(1)}\n\n{entry.published}\n\n{entry.link}"
         #api.update_status(tweet_text)
-        print(tweet_text)
+        m.toot(toot_text)
+        print(toot_text)
 
-# if counter == 0:
-#     print("[*] No new posts found.")
-# else:
-#     print(f"[*] {counter} new posts found.")
+
+
+print(cfg.mastodon['instance'])
